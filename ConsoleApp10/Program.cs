b022538f263f92ebc73f96910b2af39ce6f0cda4
Melisa Kılıç <mk_i3@hotmail.com>
2024-10-16 17:58:55 +0300

    Add project files.

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿
+//While döngüsü
+Console.WriteLine("Bir sayı giriniz:");
+int limit=int.Parse(Console.ReadLine());
+int sayac = 0;
+
+while (sayac<=limit)
+{
+    Console.WriteLine(sayac +"  Ben bir Patika'lıyım");
+    sayac++;
+}
+Console.WriteLine("teşekkürler");
+
+//Do while döngüsü
+Console.WriteLine("Bir sayı giriniz:");
+int limit2=int.Parse(Console.ReadLine());
+int sayac2=0;
+do
+{
+    Console.WriteLine(sayac2 + "Ben bir patikalıyım");
+    sayac2++;
+} 
+while (sayac2<=limit2);
+Console.WriteLine("Teşekkürler");
+
+//ÇIKARILAN SONUÇ
+//Do while döngüsü koşul yanlış bile olsa en az bir kez çalışır
+//do while döngüsü koşulu en son ister ama while başta ister
